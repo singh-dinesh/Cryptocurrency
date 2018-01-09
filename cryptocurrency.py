ad8fd8d00fa7aae37ee181cfca724230c97d198f
Dinesh Singh <dineshsingh9375@gmail.com>
2018-01-10 00:05:58 +0530

    Current cryptocurrency status function implemented

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -11,22 +11,46 @@ A short module that can save your precious time that you spend checking the
 right time to buy a cryptocurrency.
 """
 
-import sys
+import sys, time
 import requests
 from bs4 import BeautifulSoup
 
-def get_status(rate):
-    # Upcoming code
+
+def get_status():
+    URL = 'https://www.coinome.com/exchange'
+    content = requests.get(URL).content
+    soup = BeautifulSoup(content, 'html.parser')
+
+    Currencies = soup.findAll('span', {'class': 'last-market-rate-b'})
+    price = []
+    for currency in Currencies:
+        price.append(currency.text.replace(',',''))
+
+    print('-'*80)
+    print('LTC: {}, BTC: {} and BCH: {}'.format(*price))
+
+
+def run(RATE):
+    print('Getting Current Status:')
+    while True:
+        try:
+            get_status()
+            time.sleep(RATE)
+        except :
+            print('Trying to reconnect...')
+            time.sleep(10)
+
 
 def main():
     try:
+        if len(sys.argv) > 2:
+            raise IndexError()
         REFRESH_RATE = int(sys.argv[1])
     except IndexError:
         print('Usage: cryptocurrency.py <argument>')
-        sys.exit(1)
     except ValueError:
         print('Error: Argument provided is not an integer !! Try again.')
-        sys.exit(2)
+    run(REFRESH_RATE)
 
 
 if __name__ == '__main__': main()
