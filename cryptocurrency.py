c5d118ac5c220ed3d665c233e9dcb7d190de6877
Dinesh Singh <dineshsingh9375@gmail.com>
2018-01-12 00:04:32 +0530

    Bell alert added for getting notified whenever price rises

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -10,12 +10,19 @@ __license__ = 'MIT'
 A short module that can save your precious time that you spend checking the
 right time to buy a cryptocurrency.
 """
-
-import sys, time
 import requests
+import sys, time
+from pygame import mixer
 from bs4 import BeautifulSoup
 
+# Initialized the mixer object
+mixer.init()
+alert=mixer.Sound('bell.wav')
 
+# For testing purpose, DEFAULT_LTC is set
+DEFAULT_LTC = 17000.0
+
+# What's the current cryptocurrency status, fetch it for me -> get_status()
 def get_status():
     URL = 'https://www.coinome.com/exchange'
     content = requests.get(URL).content
@@ -24,19 +31,26 @@ def get_status():
     Currencies = soup.findAll('span', {'class': 'last-market-rate-b'})
     price = []
     for currency in Currencies:
-        price.append(currency.text.replace(',',''))
+        price.append(float(currency.text.replace(',','')))
 
     print('-'*80)
-    print('LTC: {}, BTC: {} and BCH: {}'.format(*price))
+    print('BTC: {}, BCH: {} and LTC: {}'.format(*price))
+
+    global DEFAULT_LTC
+    if DEFAULT_LTC < price[2]:
+        DEFAULT_LTC = price[2]
+        print('*LTC price increased to {}'.format(price[2]))
+        alert.play()
 
 
 def run(RATE):
     print('Getting Current Status:')
+    print("o_o Watching LTC")
     while True:
         try:
             get_status()
             time.sleep(RATE)
-        except :
+        except:
             print('Trying to reconnect...')
             time.sleep(10)
 
