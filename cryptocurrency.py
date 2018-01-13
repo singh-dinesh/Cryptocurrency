63842418ff2a565cce50905c00df6336ca9eba5b
Dinesh Singh <dineshsingh9375@gmail.com>
2018-01-13 21:20:22 +0530

    Windows 10 notification implemented for better user experience

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -14,6 +14,7 @@ import requests
 import sys, time
 from pygame import mixer
 from bs4 import BeautifulSoup
+from win10toast import ToastNotifier
 
 # Initialized the mixer object
 mixer.init()
@@ -21,6 +22,18 @@ alert=mixer.Sound('bell.wav')
 
 # For testing purpose, DEFAULT_LTC is set
 DEFAULT_LTC = 17000.0
+
+def notify(price):
+    toast = ToastNotifier()
+
+    #Title of the notification
+    title = 'LTC price rose to {}'.format(price)
+
+    # Description of the notificatio of windows
+    description = 'For best profit, buy if price below 17000'
+
+    # Display the notification
+    toast.show_toast(title, description)
 
 # What's the current cryptocurrency status, fetch it for me -> get_status()
 def get_status():
@@ -41,6 +54,7 @@ def get_status():
         DEFAULT_LTC = price[2]
         print('*LTC price increased to {}'.format(price[2]))
         alert.play()
+        notify(DEFAULT_LTC)
 
 
 def run(RATE):
@@ -60,11 +74,11 @@ def main():
         if len(sys.argv) > 2:
             raise IndexError()
         REFRESH_RATE = int(sys.argv[1])
+        run(REFRESH_RATE)
     except IndexError:
         print('Usage: cryptocurrency.py <argument>')
     except ValueError:
         print('Error: Argument provided is not an integer !! Try again.')
-    run(REFRESH_RATE)
 
 
 if __name__ == '__main__': main()
